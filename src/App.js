c5ee389e0b625f5b9152f9df72b64aeef74613d7
jellyfish <jvamsif223@gmail.com>
2021-06-14 18:46:04 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import 'rsuite/dist/styles/rsuite-default.css'
-import './styles/main.scss'
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+import { Switch } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
