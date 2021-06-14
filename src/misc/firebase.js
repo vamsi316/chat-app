a98ff44b9c1d668708034d514d40e2874fe3b8b2
jellyfish <jvamsif223@gmail.com>
2021-06-14 18:07:40 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCyROqwvVxBN7ve6G0EOOcJDGKXOJ3ZV6Q",
+    authDomain: "chat-web-app-a4def.firebaseapp.com",
+    projectId: "chat-web-app-a4def",
+    storageBucket: "chat-web-app-a4def.appspot.com",
+    messagingSenderId: "1071732006554",
+    appId: "1:1071732006554:web:4cb08f8454161fe3fe8923"
+  };
+
+const app = firebase.initializeApp(config);
